1b45b26faf004f523f5eeaf33ee5059365c55efb
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-01 11:53:28 +0430

    Add Env varables to ipcRenderer

--- main/preload.js
+++ main/preload.js
@@ -1,7 +1,8 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer, remote } = require('electron')
 
 // Since we disabled nodeIntegration we can reintroduce
 // needed node functionality here
 process.once('loaded', () => {
   global.ipcRenderer = ipcRenderer
+  global.process = { env: process.env }
 })
