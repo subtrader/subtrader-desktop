8203ca28208774fc23f61faa83dd9aba5fa44b05
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-01 11:52:25 +0430

    Add init setting page

---
+++ renderer/pages/setting.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+
+export default class extends Component {
+  state = {
+    input: '',
+    message: null,
+  }
+
+  render() {
+    return <div>This is Page1</div>
+  }
+}
