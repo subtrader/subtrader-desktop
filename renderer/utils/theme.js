2d05767ac494c4c7f37a454938cc73cf2e3863a0
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-01 11:51:39 +0430

    Add theme rules

---
+++ renderer/utils/theme.js
@@ -0,0 +1,12 @@
+export default {
+  //fonts
+  mainFont:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif',
+
+  //sizes
+  innerPageSidePadding: 32,
+
+  //colors
+  mainBgColor: '#E5E5E5',
+  sidebarBg: '#6D9BF4',
+}
