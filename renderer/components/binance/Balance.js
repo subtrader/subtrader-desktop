72f8a227a61d1bb37007156c4d80adce0a4082cc
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-01 11:50:05 +0430

    Add balance page

---
+++ renderer/components/binance/Balance.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+class Balance extends Component {
+  state = {
+    client: '',
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Hello WOrlds</div>
+      </div>
+    )
+  }
+}
+
+export default Balance
