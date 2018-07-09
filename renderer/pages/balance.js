d6601dbbebcb1cf2f8d810e9db20a249c26fb619
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-09 22:05:48 +0430

    Fix binance api

--- renderer/pages/balance.js
+++ renderer/pages/balance.js
@@ -1,18 +1,32 @@
 import { Component } from 'react'
-import Balance from '../components/binance/Balance'
+// import Balance from '../components/binance/Balance'
 
 export default class extends Component {
   state = {
-    input: '',
-    message: null,
+    price: null,
   }
+  componentDidMount() {
+    global.ipcRenderer.on('price-reply', this.handlePrice)
+    global.ipcRenderer.send('price', 1)
+  }
+  componentWillUnmount() {
+    // stop listening the channel message
+    global.ipcRenderer.removeListener('price', this.handlePrice)
+  }
+  handlePrice = (event, price) => {
+    console.log('price', price)
+    this.setState({ price })
 
+    // let mainValue = global.ipcRenderer.sendSync('price', 3)
+    // console.log(mainValue)
+  }
   render() {
     return (
       <div>
         This is Balance Page
         <div>
-          <Balance />
+          {/* <Balance /> */}
+          <div>{this.state.price && <p>{this.state.price}</p>}</div>
         </div>
       </div>
     )
