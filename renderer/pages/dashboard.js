a8868d5fff1ff933ce30964ed7fc1a9be50a0fcd
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-09 22:06:34 +0430

    Add Condition section

--- renderer/pages/dashboard.js
+++ renderer/pages/dashboard.js
@@ -1,30 +1,90 @@
 import { Component } from 'react'
+import ConditionItem from '../components/ConditionItem'
+import produce from 'immer'
 
 export default class extends Component {
   state = {
-    input: '',
-    price: null,
+    conditions: null,
   }
   componentDidMount() {
-    global.ipcRenderer.on('price-reply', this.handlePrice)
-    global.ipcRenderer.send('price', 1)
+    global.ipcRenderer.on('ConditonsRestore', this.initCoditions)
+    global.ipcRenderer.send('ConditonsRestore', '')
+    // this.setState({
+    //   conditions: [
+    //     {
+    //       coin: 'BTC',
+    //       price: 6700,
+    //       op: '>',
+    //       balanceList: [
+    //         { name: 'BTC', usage: true },
+    //         { name: 'NEO', usage: false },
+    //         { name: 'ETH', usage: true },
+    //       ],
+    //     },
+    //     {
+    //       coin: 'ETH',
+    //       price: 510,
+    //       op: '>',
+    //       balanceList: [
+    //         { name: 'ETH', usage: true },
+    //         { name: 'NEO', usage: false },
+    //         { name: 'BTC', usage: true },
+    //       ],
+    //     },
+    //     {
+    //       coin: 'BNB',
+    //       price: 13,
+    //       op: '>',
+    //       balanceList: [
+    //         { name: 'NEO', usage: false },
+    //         { name: 'BTC', usage: true },
+    //         { name: 'ETH', usage: false },
+    //       ],
+    //     },
+    //   ],
+    // })
   }
-  componentWillUnmount() {
-    // stop listening the channel message
-    global.ipcRenderer.removeListener('price', this.handlePrice)
-  }
-  handlePrice = (event, price) => {
-    console.log('price', price)
-    this.setState({ price })
 
-    // let mainValue = global.ipcRenderer.sendSync('price', 3)
-    // console.log(mainValue)
+  componentWillUnmount() {
+    global.ipcRenderer.removeListener('ConditonsRestore', this.handlePrice)
   }
+
+  initCoditions = (event, conditions) => {
+    console.log(conditions)
+    this.setState({ conditions })
+  }
+
+  restoreConditions = () => {
+    global.ipcRenderer.send('ConditonsRestore', '')
+  }
+
+  onClickBalanceCoin = (itemIndex, balanceIndex) => {
+    console.log(itemIndex, balanceIndex)
+    const nextState = produce(this.state, draft => {
+      const is = draft.conditions[itemIndex].balanceList[balanceIndex]
+
+      amdlk.usage = !amdlk.usage
+    })
+    this.setState(nextState)
+    global.ipcRenderer.send('ConditonsSave', nextState.conditions)
+  }
+
   render() {
     return (
       <div>
-        <div>This is Page2</div>
-        <div>{this.state.price && <p>{this.state.price}</p>}</div>
+        <div onClick={() => this.restoreConditions()}>My Conditions</div>
+        {this.state.conditions &&
+          this.state.conditions.map((p, i) => (
+            <ConditionItem
+              balanceList={p.balanceList}
+              key={i}
+              index={i}
+              onClickBalanceCoin={this.onClickBalanceCoin}
+            >
+              {p.coin}
+              <span>({p.price})</span>
+            </ConditionItem>
+          ))}
       </div>
     )
   }
