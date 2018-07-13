a3665116502189bc291d599574a8707608b6d138
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-13 18:06:52 +0430

    Add Condition list store test

--- renderer/pages/dashboard.js
+++ renderer/pages/dashboard.js
@@ -1,48 +1,42 @@
 import { Component } from 'react'
-import ConditionItem from '../components/ConditionItem'
 import produce from 'immer'
+
+import ConditionItem from '../components/ConditionItem'
+import Selector from '../components/shared/Selector'
+import SelectorMin from '../components/shared/SelectorMin'
 
 export default class extends Component {
   state = {
     conditions: null,
   }
+
+  render() {
+    return (
+      <div>
+        <div onClick={() => this.restoreConditions()}>My Conditions</div>
+        <button onClick={() => this.clearConditions()}>rest</button>
+        <button onClick={() => this.testDataConditions()}>test data</button>
+        <hr />
+        <SelectorMin />
+        {this.state.conditions &&
+          this.state.conditions.map((p, i) => (
+            <ConditionItem
+              balanceList={p.balanceList}
+              key={i}
+              index={i}
+              onClickBalanceCoin={this.onClickBalanceCoin}
+            >
+              {p.coin}
+              <span>({p.price})</span>
+            </ConditionItem>
+          ))}
+      </div>
+    )
+  }
+
   componentDidMount() {
     global.ipcRenderer.on('ConditonsRestore', this.initCoditions)
     global.ipcRenderer.send('ConditonsRestore', '')
-    // this.setState({
-    //   conditions: [
-    //     {
-    //       coin: 'BTC',
-    //       price: 6700,
-    //       op: '>',
-    //       balanceList: [
-    //         { name: 'BTC', usage: true },
-    //         { name: 'NEO', usage: false },
-    //         { name: 'ETH', usage: true },
-    //       ],
-    //     },
-    //     {
-    //       coin: 'ETH',
-    //       price: 510,
-    //       op: '>',
-    //       balanceList: [
-    //         { name: 'ETH', usage: true },
-    //         { name: 'NEO', usage: false },
-    //         { name: 'BTC', usage: true },
-    //       ],
-    //     },
-    //     {
-    //       coin: 'BNB',
-    //       price: 13,
-    //       op: '>',
-    //       balanceList: [
-    //         { name: 'NEO', usage: false },
-    //         { name: 'BTC', usage: true },
-    //         { name: 'ETH', usage: false },
-    //       ],
-    //     },
-    //   ],
-    // })
   }
 
   componentWillUnmount() {
@@ -58,34 +52,61 @@ export default class extends Component {
     global.ipcRenderer.send('ConditonsRestore', '')
   }
 
+  clearConditions = () => {
+    if (confirm('Are you sure?')) {
+      global.ipcRenderer.send('ConditonsSave', [])
+      global.ipcRenderer.send('ConditonsRestore', '')
+    }
+  }
+
+  testDataConditions = () => {
+    if (confirm('Are you sure?')) {
+      this.setState({
+        conditions: [
+          {
+            coin: 'BTC',
+            price: 6700,
+            op: '>',
+            balanceList: [
+              { name: 'BTC', usage: true },
+              { name: 'NEO', usage: false },
+              { name: 'ETH', usage: true },
+            ],
+          },
+          {
+            coin: 'ETH',
+            price: 510,
+            op: '>',
+            balanceList: [
+              { name: 'ETH', usage: true },
+              { name: 'NEO', usage: false },
+              { name: 'BTC', usage: true },
+            ],
+          },
+          {
+            coin: 'BNB',
+            price: 13,
+            op: '>',
+            balanceList: [
+              { name: 'NEO', usage: false },
+              { name: 'BTC', usage: true },
+              { name: 'ETH', usage: false },
+            ],
+          },
+        ],
+      })
+      global.ipcRenderer.send('ConditonsSave', this.state.conditions)
+    }
+  }
+
   onClickBalanceCoin = (itemIndex, balanceIndex) => {
     console.log(itemIndex, balanceIndex)
     const nextState = produce(this.state, draft => {
-      const is = draft.conditions[itemIndex].balanceList[balanceIndex]
+      const isInfected = draft.conditions[itemIndex].balanceList[balanceIndex]
 
-      amdlk.usage = !amdlk.usage
+      isInfected.usage = !isInfected.usage
     })
     this.setState(nextState)
     global.ipcRenderer.send('ConditonsSave', nextState.conditions)
-  }
-
-  render() {
-    return (
-      <div>
-        <div onClick={() => this.restoreConditions()}>My Conditions</div>
-        {this.state.conditions &&
-          this.state.conditions.map((p, i) => (
-            <ConditionItem
-              balanceList={p.balanceList}
-              key={i}
-              index={i}
-              onClickBalanceCoin={this.onClickBalanceCoin}
-            >
-              {p.coin}
-              <span>({p.price})</span>
-            </ConditionItem>
-          ))}
-      </div>
-    )
   }
 }
